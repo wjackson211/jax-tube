4ec6cf1f5432e11ca1edadafd1ffc0c173accddf
wjackson211 <wjackson211@gmail.com>
2019-06-07 17:53:19 -0500

    adding first volume knob control

--- src/App.js
+++ src/App.js
@@ -2,23 +2,19 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Knob from './components/knob';
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Knob
+            numTicks={125}
+            degrees={270}
+            min={1}
+            max={100}
+            value={0}
+            //onChange={this.handleChange}
+          />
     </div>
   );
 }
